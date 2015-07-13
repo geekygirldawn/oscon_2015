ca420992392d3b84b78ab587c71b6ef7cf8f287b
Dawn M. Foster <dawn@dawnfoster.com>
2015-07-13 10:09:49 +0100

    new version with some minor changes and to make sure that the version in the repo is up to date

--- r-files/plot_network.R
+++ r-files/plot_network.R
@@ -9,7 +9,7 @@ library(igraph)
 # Load the data into a table (comma separated with a header line)
 
 mailing_list<-read.table("~/gitrepos/oscon_2015/data/network_output.csv", sep=',', 
-                                    header=T)
+                                    header=TRUE)
 
 # Look at table and verify that it looks good
 
@@ -24,28 +24,32 @@ mailing_list.graph <- graph.edgelist(mailing_list.mat, directed = TRUE)
 
 # Convert duplicate conversations to weights. Gives each exchange from person a to 
 # person b the weight of one (E stands for an edge sequence, which is a way to link
-# people together). Converts it into a simple graph that does not have loops or duplicate
+# people together). 
+# Converts it into a simple graph that does not have loops or duplicate
 # connections (edges) between people. Combine these edge attributes (edge.attr.comb)
 # by summing the weight to give a number that represents the number of times
 # person a replied to person b.
 
 E(mailing_list.graph)$weight <- 1
 mailing_list.graphw <- simplify(mailing_list.graph, edge.attr.comb=list(weight="sum"))
+?simplify
 
 # Interactive graph using tkplot that uses the weights calculated above to make
 # the arrows between people (edges) wider depending on how many times one person has
 # replied to another. The circles (nodes) representing each person become larger the
-# more connections that person has to others within the network (betweeness centrality).
+# more connections that person has to others within the network (degree centrality).
 
 tkplot(mailing_list.graphw, vertex.label.color="black", edge.color="darkslategray",
        edge.width=E(mailing_list.graphw)$weight/3, edge.arrow.size=.5, 
        vertex.size=degree(mailing_list.graph)/2)
 
-# export a format of the data with weights that can be imported into other visualization sw
+# export a format of the data with weights that can be imported into other 
+# visualization sw
 
 write.graph(mailing_list.graphw, "/tmp/network.dot", format=c("dot"))
 
-# STOP here. The stuff below is just bonus material for those who want a couple other alternatives.
+# STOP here. The stuff below is just bonus material for those who want a couple 
+# other alternatives.
 ###################
 
 
@@ -63,5 +67,7 @@ tkplot(mailing_list.graphw, vertex.label.color="black", edge.color="darkslategra
 # Static Plot for reference - it's hard to make these look awesome.
 
 plot.igraph(mailing_list.graphw,vertex.label=V(mailing_list.graph)$name,
-            layout=layout.fruchterman.reingold, vertex.size=20, vertex.label.color="black",
-            edge.color="black",edge.width=E(mailing_list.graphw)$weight/3, edge.arrow.size=.1)
+            layout=layout.fruchterman.reingold, vertex.size=20, 
+            vertex.label.color="black",
+            edge.color="black",edge.width=E(mailing_list.graphw)$weight/3, 
+            edge.arrow.size=.1)
